09e7df2706fe392218de05cfa9a45ffec54edc7b
leeseooo <leeseoooo@naver.com>
2022-04-10 00:37:50 +0900

    feat: 로그인 및 회원가입 폼 공통 헤더 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import MainPage from './pages/MainPage';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/main" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
