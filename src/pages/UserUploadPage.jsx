e67fd1e6e3449f795135274a3b6e13a9616b1785
leeseooo <leeseoooo@naver.com>
2022-04-26 13:18:10 +0900

    feat: UserUploadPage -> 신조어 섹션 스타일링, html 추가

--- src/pages/UserUploadPage.jsx
+++ src/pages/UserUploadPage.jsx
@@ -46,23 +46,11 @@ function UserUploadPage() {
                 카테고리 선택
               </strong>
               <div className="question__answer">
-                <input
-                  type="radio"
-                  name="카테고리"
-                  value="예능"
-                />
+                <input type="radio" name="카테고리" value="예능" />
                 <label htmlFor="카테고리">예능</label>
-                <input
-                  type="radio"
-                  name="카테고리"
-                  value="드라마"
-                />
+                <input type="radio" name="카테고리" value="드라마" />
                 <label htmlFor="카테고리">드라마</label>
-                <input
-                  type="radio"
-                  name="카테고리"
-                  value="그 외"
-                />
+                <input type="radio" name="카테고리" value="그 외" />
                 <label htmlFor="카테고리">그 외</label>
               </div>
             </li>
@@ -79,14 +67,22 @@ function UserUploadPage() {
                 <span>*</span>
                 제목
               </strong>
-              <input type="text" placeholder="밈의 제목을 입력해주세요." />
+              <input
+                className="text__input"
+                type="text"
+                placeholder="밈의 제목을 입력해주세요."
+              />
             </li>
             <li className="form__list">
               <strong className="question__title">
                 <span>*</span>
                 설명
               </strong>
-              <input type="text" placeholder="등록하려는 밈을 설명해주세요." />
+              <input
+                className="text__input"
+                type="text"
+                placeholder="등록하려는 밈을 설명해주세요."
+              />
             </li>
             <li className="form__list">
               <strong className="question__title">
@@ -94,13 +90,64 @@ function UserUploadPage() {
                 키워드
               </strong>
               <input
+                className="text__input"
                 type="text"
                 placeholder="띄어쓰기 없이 입력해주세요. (최대 3개)"
               />
             </li>
           </>
         ) : typeOfMeme === "신조어" ? (
-          <div>신조어</div>
+          <>
+            <li className="form__list">
+              <strong className="question__title">
+                <span>*</span>
+                단어
+              </strong>
+              <div className="filter__text-wrapper">
+                <input
+                  className="text__input"
+                  type="text"
+                  placeholder="단어를 입력해주세요."
+                  value=""
+                />
+                <button type="button" className="filter__button">
+                  필터링
+                </button>
+              </div>
+            </li>
+            <li className="form__list">
+              <strong className="question__title">
+                <span>*</span>뜻
+              </strong>
+              <input
+                className="text__input"
+                type="text"
+                placeholder="등록하려는 밈의 뜻을 설명해주세요."
+              />
+            </li>
+            <li className="form__list">
+              <strong className="question__title">
+                <span>*</span>
+                예시
+              </strong>
+              <input
+                className="text__input"
+                type="text"
+                placeholder="등록하려는 밈의 사용 예시를 들어주세요."
+              />
+            </li>
+            <li className="form__list">
+              <strong className="question__title">
+                <span>*</span>
+                키워드
+              </strong>
+              <input
+                className="text__input"
+                type="text"
+                placeholder="띄어쓰기 없이 입력해주세요.(최대 3단어)"
+              />
+            </li>
+          </>
         ) : null}
         {typeOfMeme !== null ? (
           <BaseButton
@@ -116,7 +163,8 @@ function UserUploadPage() {
 }
 
 const Form = styled.form`
-  height: 100vh;
+margin-top:50px;
+  height: fit-content;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -133,26 +181,29 @@ const Form = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
+
   }
   .form__list {
-    width: 50%;
-    margin: 15px auto;
+    width:570px;
+    margin: 20px 0;
   }
+
   .question__answer {
-    margin: 15px auto;
+    margin: 20px auto;
   }
   .form__list > .file + p {
     font-size: 14px;
     color: #bdbdbd;
-    margin-top: 10px;
+    margin-top: 15px;
   }
-  .form__list input + label{
+  .form__list input + label {
     padding-right: 25px;
   }
-  .form__list > input:not(.file) {
+  .form__list .text__input {
     display: block;
-    width: 400px;
-    height: 30px;
+    width:500px;
+    height: 35px;
+    padding-top:15px;
     background: none;
     border: none;
     border-bottom: 1px solid #fff;
@@ -168,7 +219,20 @@ const Form = styled.form`
     &:focus::placeholder {
       color: transparent;
     }
-    
+  }
+  .form__list .filter__button {
+    display: inline-block;
+    width:63px;
+    height:30px;
+    background-color: #828282;
+    border-radius: 20px;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+  }
+  .filter__text-wrapper {
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
