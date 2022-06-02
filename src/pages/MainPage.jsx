46c89b24f1c4216a104781d1b579503be4da60ce
leeseooo <leeseoooo@naver.com>
2022-06-02 16:54:08 +0900

    feat: 단어 랭킹 api 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,27 +4,21 @@ import Footer from '../component/Footer';
 import FirstContainer from '../component/mainpage/FirstContainer';
 import RankingContainer from '../component/mainpage/RankingContainer';
 import styled from 'styled-components';
-// import { useRecoilState } from 'recoil';
-// import { isLoginState } from 'stores';
+import { getMemeRanking } from '../api/main';
 
 export default function MainPage() {
-  // const isLogin = useRecoilState(isLoginState);
   const [wordRankingList, setWordRankingList] = useState([]);
   const [hashTagList, setHashTagList] = useState([]);
 
   useEffect(() => {
-    setWordRankingList([
-      {
-        id: 1,
-        word: '어쩔티비 저쩔티비 시크릿쥬쥬 삼성티비',
-      },
-      { id: 2, word: '신기방기 뿡뿡방기 청기백기 동방신기' },
-      { id: 3, word: '알잘딱깔센' },
-      { id: 4, word: '그냥 꼬옥 안아주면 되' },
-      { id: 5, word: '롬곡옾눞' },
-    ]);
+    fetchWordRankingList();
     setHashTagList(['#어쩔티비', '#관짝춤', '#신기방기뿡뿡방기']);
   }, []);
+
+  const fetchWordRankingList = async () => {
+    const { data } = await getMemeRanking();
+    setWordRankingList(data.content);
+  };
 
   return (
     <>
